dc32a67bef8a0ca0340ece3d9a5ebf7bd0e1bc4f
Adesina <adeshiname@gmail.com>
2023-09-18 19:59:39 +0100

    Add DB Context and connection Strings

---
+++ eagles-food-backend/Data/LunchDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eagles_food_backend.Data
+{
+    public class LunchDbContext : DbContext
+    {
+        public LunchDbContext(DbContextOptions<LunchDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
