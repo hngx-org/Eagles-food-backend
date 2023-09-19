b884677cf8d7cababf0c0db22c8b3c494565cb6f
Adesina <adeshiname@gmail.com>
2023-09-19 09:42:15 +0100

    Migrations

---
+++ eagles-food-backend/Migrations/20230919084122_StaffBankAccount.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace eagles_food_backend.Migrations
+{
+    public partial class StaffBankAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Number = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Bank_Name = table.Column<string>(type: "longtext", nullable: false),
+                    Bank_Code = table.Column<string>(type: "longtext", nullable: false),
+                    User_Id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccounts_Users_User_Id",
+                        column: x => x.User_Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Staffs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Staff_Id = table.Column<string>(type: "longtext", nullable: false),
+                    Role = table.Column<string>(type: "longtext", nullable: false),
+                    Org_Id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staffs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Staffs_Organizations_Org_Id",
+                        column: x => x.Org_Id,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Staff_Wallets",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Balance = table.Column<double>(type: "double", nullable: false),
+                    Staff_Id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff_Wallets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Staff_Wallets_Staffs_Staff_Id",
+                        column: x => x.Staff_Id,
+                        principalTable: "Staffs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_User_Id",
+                table: "BankAccounts",
+                column: "User_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_Wallets_Staff_Id",
+                table: "Staff_Wallets",
+                column: "Staff_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staffs_Org_Id",
+                table: "Staffs",
+                column: "Org_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "Staff_Wallets");
+
+            migrationBuilder.DropTable(
+                name: "Staffs");
+        }
+    }
+}
