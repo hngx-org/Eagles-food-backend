0e2db9151ba58d9800b30ffb0c9095b05018b58e
Bolu <bolubee95@gmail.com>
2023-10-13 14:44:08 +0100

    resolved merge errors

--- eagles-food-backend/Services/UserRepository/IUserRepository.cs
+++ eagles-food-backend/Services/UserRepository/IUserRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace eagles_food_backend.Services.UserServices
+﻿using eagles_food_backend.Domains.DTOs;
+using eagles_food_backend.Domains.Models;
+
+namespace eagles_food_backend.Services.UserServices
 {
     public interface IUserRepository
     {
